dfb523038b4afd9b95e5422224883db94a4d1f83
Frederick Kaucher <frederick.kaucher@gmail.com>
2021-10-22 17:33:40 -0600

    fixing issue with  jets repo

---
+++ src/com/SkillDistillery/jets/app/JetsApplication.java
@@ -0,0 +1,10 @@
+package com.SkillDistillery.jets.app;
+
+public class JetsApplication {
+	private Airfield airfield;
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/SkillDistillery/jets/entities/Jet.java
@@ -0,0 +1,5 @@
+package com.SkillDistillery.jets.entities;
+
+public abstract class Jet {
+
+}
